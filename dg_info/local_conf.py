72bf3658861bee9eae8056c47fcf50c86a8a69cd
Sokirlov <64358905+Sokirlov@users.noreply.github.com>
2021-07-27 17:05:38 +0300

    pub ver 0.121

--- dg_info/local_conf.py
+++ dg_info/local_conf.py
@@ -7,16 +7,25 @@ SECRET_KEY = 'django-insecure-ja4f&pac_+&-*09m1*--%g7vm7!%0s%1-+_4a6()yic8zk=u)#
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.37.71', '127.0.0.1', '109.86.40.120']
+ALLOWED_HOSTS = ['192.168.37.71', '127.0.0.1', '192.168.37.125']
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'dgf',
+            'USER' : 'root',
+            'PASSWORD' : 'root',
+            'default-character-set' : 'utf8',
+            'HOST' : 'localhost',
+            # 'PORT' : '5432',
+        }
 }
 
 STATIC_URL = '/static/'
